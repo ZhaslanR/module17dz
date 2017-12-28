1d7486c42e5399dc831df4c9978d9df8dd1faa68
Жаслан Рыспеков <zhaslan_12@bk.ru>
2017-12-28 21:00:34 +0600

    1

---
+++ module17dz/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module17dz
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public void Save()
+        {
+            using (StreamWriter writer = new StreamWriter("file.txt", true))
+            {
+                writer.WriteLine(Title);
+                writer.WriteLine(Link);
+                writer.WriteLine(Description);
+                writer.WriteLine(PubDate);
+            }
+        }
+    }
+}
---
+++ module17dz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace module17dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            XmlDocument xmlDocument = new XmlDocument();
+
+            const string path = @"https://habrahabr.ru/rss/interesting/";
+            xmlDocument.Load(path);
+            XmlNode root = xmlDocument.LastChild.LastChild;
+            foreach (XmlNode node in root)
+            {
+                if (node.Name == "item")
+                {
+                    Item item = new Item();
+
+                    var nodes = node.ChildNodes;
+                    foreach (XmlNode childNode in nodes)
+                    {
+                        if (childNode.Name == "title") item.Title = childNode.InnerText;
+                        else if (childNode.Name == "link") item.Link = childNode.InnerText;
+                        else if (childNode.Name == "desription") item.Description = childNode.InnerText;
+                        else if (childNode.Name == "pubDate") item.PubDate = DateTime.Parse(childNode.InnerText);
+                    }
+                    items.Add(item);
+                }
+            }
+
+            for (int i = 0; i < items.Count; i++) items[i].Save();
+
+            Console.ReadLine();
+        }
+    }
+}
